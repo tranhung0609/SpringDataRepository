d7e62bd3b81523feb1ee1629aebbb32ef83bb086
tranhung0609.github.io <trvanhung.nhk.0609@gmail.com>
2022-06-16 16:38:30 +0700

    done

--- src/main/java/controller/ProvinceController.java
+++ src/main/java/controller/ProvinceController.java
@@ -4,6 +4,7 @@ import model.Province;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -32,6 +33,30 @@ IProvinceService provinceService;
     public ModelAndView save(Province province){
         provinceService.save(province);
         ModelAndView modelAndView = new ModelAndView("redirect:/provinces");
+        return modelAndView;
+    }
+//    @GetMapping("/edit/{id}")
+//    public ModelAndView showEditForm(@PathVariable Long id){
+//        ModelAndView modelAndView = new ModelAndView("/province/edit");
+//        Province province = provinceService.findById(id).get();
+//        modelAndView.addObject("pro" , provinceService);
+//        return modelAndView;
+//    }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView showEditForm(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("/province/edit");
+        Province province = provinceService.findById(id).get();
+        modelAndView.addObject("pro", province);
+        return modelAndView;
+    }
+    @PostMapping("/edit/{id}")
+    public ModelAndView update(@PathVariable Long id, Province province) {
+        ModelAndView modelAndView = new ModelAndView("redirect:/provinces");
+//        Province oldProvince = provinceService.findById(id).get();
+//        oldProvince.setDescription(province.getDescription());
+//        oldProvince.setName(province.getName());
+        provinceService.save(province);
         return modelAndView;
     }
 }
